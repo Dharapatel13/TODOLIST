cbf41d5bebcf2c0c8111cb2e65032539afea3ee9
Dharapatel13 <https://github.com/Dharapatel13/To-Do-List.git>
2021-02-18 18:22:28 +0530

    working

--- todo.js
+++ todo.js
@@ -1,9 +1,7 @@
 
-
-      //var tasks=[];
-      //ADD TODOLIST
-      function on_addbtn_click()
+      function onAddBtnClick()
       {
+       
         var task_input=document.getElementById("inpt").value;
           if(task_input==="")
           {
@@ -12,31 +10,50 @@
           else{
            var ul=document.getElementById("myList");
            const li=document.createElement("li");
-           const span=document.createElement("span");     
+           const span=document.createElement("span");  
+           const editBtn = document.createElement('button');
+          editBtn.textContent = 'EDIT';
+          editBtn.className="libtn";
+          const dtlBtn = document.createElement('button');
+          dtlBtn.textContent = 'DETELE';
+          dtlBtn.className="libtn";
+          const div=document.createElement("div")
+          const check=document.createElement("button");
+          check.textContent="<i class='fas fa-check'></i>";
+         
+         
            li.appendChild(span);
-           span.innerHTML=task_input+"<button id='delete'  class='libtn'  >DELETE</button>"+"<button id='edit' class='libtn'>EDIT</button>";
-           task_input.value='';
-           ul.appendChild(li);
+           li.appendChild(check);
+           li.appendChild(editBtn);
+           li.appendChild(dtlBtn);
+        
+           span.innerHTML=task_input;
+           task_input.value="";
+           ul.appendChild(div);
+           div.appendChild(li);
+          
      
           }  
       }
-      const ul = document.querySelector('#myList');
-      
-      ul.addEventListener('click', function(event) {
+     
+      document.getElementById("myList").addEventListener('click', function(event) {
        
         if(event.target.tagName === 'BUTTON') {
           const button = event.target;
-          const span = button.parentNode;
-          const li=span.parentNode;
-          const ul = li.parentNode;
-          if(button.textContent === 'DELETE') {
-            ul.removeChild(li);   
+          console.log(button);
+          const li = button.parentNode;
+          const div = li.parentNode;
+          const ul=div.parentNode;
+       if(button.textContent === 'DETELE') {
+            ul.removeChild(div);   
          } else if(button.textContent === 'EDIT') {
             const span= li.firstElementChild;
             const input = document.createElement('input');
+            input.className="listinpt";
             input.type = 'text';
-            input.value = span.textContent;
-            li.insertBefore(input, span);
+            input.value = li.firstElementChild.textContent;
+            console.log(input.value);
+            li.insertBefore(input,span);
             li.removeChild(span);
             button.textContent = 'UPDATE';
           } else if(button.textContent === 'UPDATE') {
@@ -47,7 +64,39 @@
             li.removeChild(input);
             button.textContent = 'EDIT';
           }
+
        }
+     
+      });
+      
+     function onRstBtnClick(){
+        document.getElementById("myList").innerHTML="";
+      }
+
+
+      const show=document.getElementById("selected");
+      show.addEventListener("click",function(e){
+        const option=show.childNodes;
+        if(e.target.tagName===option)
+        {  
+          const optn = e.target;
+          const  stl= optn.parentNode;
+          if(document.textContent==="All")
+          {
+            const ul=document.getElementById("myList");
+            const div=ul.childNodes;
+            const li=div.childNodes;
+            ul.style.display="flex";
+            
+          }
+          else if(document.textContent==="Completed")
+          {
+            const checkbox=li.firstElementChild
+           
+            
+          }
+        }
+       
       });
     
    
